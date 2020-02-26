0b08cb8b592a1b931228b0a98fd076b605baba9c
viczsaurav <viczsaurav@gmail.com>
2020-02-26 21:00:01 +0100

    cleanup

--- src/main/java/com/social/network/utils/EMLParser.java
+++ src/main/java/com/social/network/utils/EMLParser.java
@@ -55,34 +55,6 @@ public class EMLParser {
 								.map(obj -> new Person(obj))
 								.collect(Collectors.toSet());
 
-			/**
-			 * If there are multiple emails for same person, we assume `Name` as the unique.
-			 * We then group emails for people with same name
-			 * So below we extract Names for both sender and recipient.
-			 */
-
-//				String senderName="";
-//				List<String> recipientNames = new ArrayList<>();
-//				for (Enumeration<Header> e = mime.getAllHeaders(); e.hasMoreElements(); ) {
-//						Header h = e.nextElement();
-//						// Get the Sender Name
-//						if (h.getName().equals("X-From")) {
-//										senderName = h.getValue();
-//						}
-//						// Get list of recipient Name
-//						 if (h.getName().equals("X-To")) {
-//								recipientNames = Arrays.asList(h.getValue().split(","))
-//																				.stream().map(String::trim)
-//																				.filter(str -> (
-//																								str.split("@").length == 1 ||  // Either Name
-//																								(Utilities.isValidEmail(str)))       // Email IDs are allowed as Name
-//										 )
-//										.collect(Collectors.toList());
-//						}
-//				}
-//				this.sender = new Person(senderName, senderEmail);
-//				this.recipientList = Person.getPersonList(recipientNames, recipientEmails);
-
 		}
 		catch (Exception e){
 			System.out.println("Failed to parse eml file: "+ path +"\n"+ e.getMessage());
